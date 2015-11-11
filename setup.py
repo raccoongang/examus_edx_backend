b0d5d5cb10ae5c468e87cbea87036cb7775ef5ed
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-11-11 21:54:07 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='examus_edx_proctor_backend',
+    version='1.0',
+    description='EDX proctoring backend for Examus service',
+    author='edX',
+    url='',
+)
